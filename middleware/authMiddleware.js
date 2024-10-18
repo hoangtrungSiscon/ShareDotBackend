863cbdd7710bafed6f83215a56d73a48d099b095
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-10-18 08:26:36 +0700

    update middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,21 +6,23 @@ const models = initModels(sequelize);
 
 const authMiddleware = async(req, res, next) => {
     const token = req.header('Authorization')?.split(" ")[1];
+
     if (!token) return res.status(401).json({ message: "No token" });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         const user = await models.users.findOne({
-            where: { userid: decoded.id },
+            where: { userid: decoded.userid },
             attributes: ['userid', 'role']
         });
-        if (!user) return res.status(401).json({ message: "Invalid token" });
+        if (!user) return res.status(401).json({ message: "Invalid token1" });
 
         req.user = user; // Lưu thông tin id và role vào req
         next();
     } catch (error) {
-        res.status(401).json({ message: "Invalid token" });
+        console.error("Token verification error:", error); 
+        res.status(401).json({ message: "Invalid token2" });
     }
 };
 
