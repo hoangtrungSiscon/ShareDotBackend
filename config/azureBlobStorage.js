5c1c8621b63c8727b436899f69e50e568e0b78d7
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-10-27 14:56:08 +0700

    function get url

--- config/azureBlobStorage.js
+++ config/azureBlobStorage.js
@@ -1,26 +1,42 @@
+// const { BlobServiceClient } = require('@azure/storage-blob');
+
+// class AzureBlobStorage {
+//   constructor(connectionString, containerName) {
+//     this.blobServiceClient = new BlobServiceClient(connectionString);
+//     this.containerClient = this.blobServiceClient.getContainerClient(containerName);
+//   }
+
+//   async uploadFile(file, fileName) {
+//     const blobClient = this.containerClient.getBlockBlobClient(fileName);
+//     await blobClient.uploadFile(file.path, {
+//       blobHTTPHeaders: {
+//         'Content-Type': file.mimetype
+//       }
+//     });
+
+//     return blobClient.url;
+//   }
+
+//   async getDownloadStream(fileName) {
+//     const blobClient = this.containerClient.getBlockBlobClient(fileName);
+//     return await blobClient.download();
+//   }
+// }
+
+// module.exports = AzureBlobStorage;
+
 const { BlobServiceClient } = require('@azure/storage-blob');
 
-class AzureBlobStorage {
-  constructor(connectionString, containerName) {
-    this.blobServiceClient = new BlobServiceClient(connectionString);
-    this.containerClient = this.blobServiceClient.getContainerClient(containerName);
-  }
+const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
 
-  async uploadFile(file, fileName) {
-    const blobClient = this.containerClient.getBlockBlobClient(fileName);
-    await blobClient.uploadFile(file.path, {
-      blobHTTPHeaders: {
-        'Content-Type': file.mimetype
-      }
-    });
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
 
-    return blobClient.url;
-  }
+const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
 
-  async getDownloadStream(fileName) {
-    const blobClient = this.containerClient.getBlockBlobClient(fileName);
-    return await blobClient.download();
-  }
+async function getBlobURL(blobName) {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    return blockBlobClient.url;
 }
 
-module.exports = AzureBlobStorage;
+module.exports = { getBlobURL };
