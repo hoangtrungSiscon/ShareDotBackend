a6a40e73308bedf0c80c4d1c0f1cc86fc0b28980
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-11-02 11:07:22 +0700

    increase view count

--- routes/documentinteraction.js
+++ routes/documentinteraction.js
@@ -7,7 +7,26 @@ const models = initModels(sequelize);
 const { Op } = require('sequelize');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/documents/:documentid/record-view', authMiddleware, async (req, res, next) => {
+router.get('/documents/:documentid/status', authMiddleware, async (req, res, next) => {
+    const { documentid } = req.params;
+    const user = req.user;
+    try {
+        const [record, created] = await models.documentinteractions.findOrCreate({
+            where: { documentid: documentid, userid: user.userid },
+            defaults: {
+                documentid: documentid,
+                userid: user.userid
+            }
+        })
+
+        res.status(200).json(record);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+})
+
+router.put('/documents/:documentid/increase-view', authMiddleware, async (req, res, next) => {
     const { documentid } = req.params;
     const user = req.user;
     try {
@@ -19,9 +38,9 @@ router.post('/documents/:documentid/record-view', authMiddleware, async (req, re
             }
         })
         
-        const incrementResult = await models.documents.increment('viewcount', { where: { documentid: documentid } });
+        await models.documents.increment('viewcount', { where: { documentid: documentid } });
 
-        res.status(200).json({ message: 'View recorded successfully' });
+        res.status(200).json(record);
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: 'An error occurred' });
@@ -32,12 +51,30 @@ router.put('/documents/:documentid/like', authMiddleware, async (req, res, next)
     const { documentid } = req.params;
     const user = req.user;
     try {
-        await models.documentinteractions.update(
-            { isliked: true, likedate: new Date() },
-            { where: { documentid: documentid, userid: user.userid } }
-        )
+        const hasLiked = await models.documentinteractions.findOne({
+            where: { documentid: documentid, userid: user.userid, isliked: true }
+        });
 
-        res.status(200).json({ message: 'Document liked successfully' });
+        if (!hasLiked) {
+            await models.documentinteractions.update(
+                { isliked: true, likedate: new Date() },
+                { where: { documentid: documentid, userid: user.userid } }
+            )
+
+            await models.documents.increment('likecount', { where: { documentid: documentid } });
+
+            return res.status(200).json({ message: 'Document liked successfully' });
+        }
+        else {
+            await models.documentinteractions.update(
+                { isliked: false, likedate: null },
+                { where: { documentid: documentid, userid: user.userid } }
+            )
+
+            await models.documents.decrement('likecount', { where: { documentid: documentid } });
+
+            return res.status(200).json({ message: 'Document unliked successfully' });
+        }
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: 'An error occurred' });
@@ -48,12 +85,26 @@ router.put('/documents/:documentid/bookmark', authMiddleware, async (req, res, n
     const { documentid } = req.params;
     const user = req.user;
     try {
-        await models.documentinteractions.update(
-            { isbookmarked: true, bookmarkdate: new Date() },
-            { where: { documentid: documentid, userid: user.userid } }
-        )
+        const hasBookmarked = await models.documentinteractions.findOne({
+            where: { documentid: documentid, userid: user.userid, isbookmarked: true }
+        });
 
-        res.status(200).json({ message: 'Document bookmarked successfully' });
+        if (!hasBookmarked) {
+            await models.documentinteractions.update(
+                { isbookmarked: true, bookmarkdate: new Date() },
+                { where: { documentid: documentid, userid: user.userid } }
+            )
+
+            return res.status(200).json({ message: 'Document bookmarked successfully' });
+        }
+        else {
+            await models.documentinteractions.update(
+                { isbookmarked: false, bookmarkdate: null },
+                { where: { documentid: documentid, userid: user.userid } }
+            )
+
+            return res.status(200).json({ message: 'Document unbookmarked successfully' });
+        }
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: 'An error occurred' });
