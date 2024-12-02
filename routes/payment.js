2e93a7953d3f660ee6f3edfd4603c701222fb06c
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-12-02 09:02:37 +0700

    get invoices

--- routes/payment.js
+++ routes/payment.js
@@ -29,4 +29,55 @@ router.post('/paypal/capture-order', async (req, res, next)=>{
     }
 });
 
+router.get('/invoices', authMiddleware, async (req, res, next)=>{
+    const user = req.user
+    const {page = 1, limit = 10, status, sortby = 'createdat', sortorder = 'DESC'} = req.query
+    try {
+        whereClause = [
+            { userid: user.userid },
+        ]
+        if (status) {
+            whereClause.push({ status: status })
+        }
+        const { count, rows } = await models.payments.findAndCountAll({
+            where: whereClause,
+            offset: (page - 1) * limit,
+            limit: limit,
+            order: [[sortby, sortorder]],
+        })
+
+        res.status(200).json({
+            totalItems: count,
+            invoices: rows,
+            currentPage: parseInt(page),
+            totalPages: Math.ceil(count / limit)
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'An error occurred. Cannot get invoices' });
+    }
+});
+
+router.get('/invoices/:paymentid', authMiddleware, async (req, res, next)=>{
+    const user = req.user
+    const {paymentid} = req.params
+    try {
+        const payment_info = await models.payments.findOne({
+            where: { userid: user.userid, paymentid: paymentid },
+            include: [
+                {
+                    model: models.users,
+                    as: 'user',
+                    attributes: ['fullname', 'email', 'userid']
+                }
+            ]
+        })
+
+        res.status(200).json(payment_info)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'An error occurred. Cannot get invoices' });
+    }
+});
+
 module.exports = router;
