b3b9e4a208c9417c9b83075c867ff32dd0b93417
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-12-12 11:10:20 +0700

    update

--- routes/storage.js
+++ routes/storage.js
@@ -11,6 +11,7 @@ const upload = multer();
 const path = require('path');
 const { toLowerCaseNonAccentVietnamese } = require('../functions/non-accent-vietnamese-convert');
 const {authMiddleware, identifyUser} = require('../middleware/authMiddleware');
+const Document = require('../mongodb_schemas/documents');
 
 router.get('/documents/:documentid/url', identifyUser, async (req, res) => {
     const { documentid } = req.params;
@@ -134,6 +135,78 @@ router.post('/upload-document', authMiddleware, upload.single('file'),  async (r
             documentid: document.documentid,
             uploaderid: user.userid
         })
+
+        const document_detail = await models.documents.findOne({
+            where: { documentid: document.documentid },
+            include: [
+                {
+                    model: models.uploads,
+                    as: 'uploads',
+                    required: true,
+                    include: [
+                        {
+                            model: models.users,
+                            as: 'uploader',
+                            required: true,
+                            attributes: ['fullname', 'userid', 'username']
+                        }
+                    ]
+                },
+                {
+                    model: models.chapters,
+                    as: 'chapter',
+                    required: true,
+                    include: [
+                        {
+                            model: models.categories,
+                            as: 'category',
+                            required: true,
+                            include: [
+                                {
+                                    model: models.categories,
+                                    as: 'parentcategory',
+                                    required: true,
+                                    include: [
+                                        {
+                                            model: models.mainsubjects,
+                                            as: 'mainsubject',
+                                            required: true,
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+        });
+
+        await Document.create({
+            title: document_detail.title,
+            documentid: document_detail.documentid,
+            mainsubjectid: document_detail.chapter.category.parentcategory.mainsubject.mainsubjectid,
+            mainsubjectname: document_detail.chapter.category.parentcategory.mainsubject.mainsubjectname,
+            categoryid: document_detail.chapter.category.parentcategory.categoryid,
+            categoryname: document_detail.chapter.category.parentcategory.categoryname,
+            subcategoryid: document_detail.chapter.category.categoryid,
+            subcategoryname: document_detail.chapter.category.categoryname,
+            chapterid: document_detail.chapter.chapterid,
+            chaptername: document_detail.chapter.chaptername,
+            filetype: document_detail.filetype,
+            filesize: document_detail.filesize,
+            accesslevel: document_detail.accesslevel,
+            status: document_detail.status,
+            viewcount: document_detail.viewcount,
+            pointcost: document_detail.pointcost,
+            description: document_detail.description,
+            uploaddate: document_detail.uploads[0].uploaddate,
+            filepath: document_detail.filepath,
+            uploaderid: document_detail.uploads[0].uploaderid,
+            uploadername: document_detail.uploads[0].uploader.fullname,
+            isactive: document_detail.isactive,
+            slug: document_detail.slug,
+            uploaderusername: document_detail.uploads[0].uploader.username
+        });
 
         res.status(200).json({ message: 'Document uploaded successfully' });
     } catch (error) {
